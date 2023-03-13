bc2e60f4eaa2c8673c9eee566b96f531a386c575
Martin Richman <martyrichman100@gmail.com>
2023-03-13 15:52:38 +0000

    Fix to database cluster names

--- mongo/db-init.sh
+++ mongo/db-init.sh
@@ -4,19 +4,19 @@ DELAY=30
 
 sleep 20
 
-mongo --host mongo1:27017 <<EOF
+mongo --host mongo_1:27017 <<EOF
 var config = {
     "_id": "rs0",
     "version": 1,
     "members": [
         {
             "_id": 1,
-            "host": "mongo1:27017",
+            "host": "mongo_1:27017",
             "priority": 2
         },
         {
             "_id": 2,
-            "host": "mongo2:27017",
+            "host": "mongo_2:27017",
             "priority": 1
         }
     ]
@@ -28,4 +28,4 @@ echo "Waiting for configuration to be applied. Please wait ${DELAY} seconds"
 
 sleep $DELAY
 
-mongo --host mongo1:27017 < /mongo/init.js
+mongo --host mongo_1:27017 < /mongo/init.js
