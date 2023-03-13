b73940be0919474950441a6dd37224bfde254899
Martin Richman <martyrichman100@gmail.com>
2023-03-13 15:16:05 +0000

    Cluster setup for database with data entries

---
+++ mongo/init.js
@@ -0,0 +1,16 @@
+use runnerscrispsdb;
+
+db.barcodes.insertMany([
+  { '_id': 1, 'barcode': '10fb29c7aa', 'vouchercode': '10OFFBAG'},
+  { '_id': 2, 'barcode': 'aa4329d6e4', 'vouchercode': '10OFFBAG'},
+  { '_id': 3, 'barcode': 'd87a21436b', 'vouchercode': '10OFFBAG'},
+  { '_id': 4, 'barcode': '1d9a681b38', 'vouchercode': 'FREEBALL'},
+  { '_id': 5, 'barcode': '70530ece82', 'vouchercode': '10OFFBAG'},
+  { '_id': 6, 'barcode': 'dedc129eb9', 'vouchercode': '10OFFBAG'},
+  { '_id': 7, 'barcode': '07f2756914', 'vouchercode': '10OFFBAG'},
+  { '_id': 8, 'barcode': '73b84113c0', 'vouchercode': '10OFFBAG'},
+  { '_id': 9, 'barcode': 'fb167f1bdf', 'vouchercode': '10OFFBAG'},
+  { '_id': 10, 'barcode': 'fb167f1bdf', 'vouchercode': '10OFFBAG'},
+  { '_id': 11, 'barcode': 'd48eb6bb67', 'vouchercode': '10OFFBAG'}
+]);
+
