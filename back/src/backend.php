b73ad8cbb3097c7b56438683fd31fe49f6edef08
Martin Richman <martyrichman100@gmail.com>
2023-03-15 13:20:34 +0000

    Changed form directory for POST

--- back/src/backend.php
+++ back/src/backend.php
@@ -1,10 +1,6 @@
 <html>
 <body>
-
-Name: <?php echo $_POST["name"]; ?><br>
-E-mail: <?php echo $_POST["email"]; ?><br>
-Address: <?php echo $_POST["address"]; ?><br>
-Best Player: <?php echo $_POST["bestplayer"]; ?>
+This is the second load balancer
 
 </body>
 </html>
